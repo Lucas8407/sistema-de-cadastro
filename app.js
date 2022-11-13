1668f629b1bf604a28fdfb9c3a1111d8d6876e16
Lucas Beltrame <lucas.accorsi.beltrame@gmail.com>
2022-11-13 14:11:52 -0300

    voltando pro github

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("aplicação conectando ao banco");
 //mais um console de teste
 console.log("commit do paulo da alura sem a gente ver");
+console.log("voltando pro github!");
