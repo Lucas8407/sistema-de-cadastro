dd6a5e14b5ee4684324088133ca0526df6faa54b
Lucas8407 <45924839+Lucas8407@users.noreply.github.com>
2022-11-13 14:03:14 -0300

    commit teste pra pull

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("aplicação conectando ao banco");
+//mais um console de teste
+console.log("commit do paulo da alura sem a gente ver");
